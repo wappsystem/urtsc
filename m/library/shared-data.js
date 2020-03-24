f7cde2e8c00d4dc447863ea692356fddabee8b48
Gunnar Unger <gunnar.unger@gmail.com>
2020-03-24 16:15:05 +1100

    update

--- m/library/shared-data.js
+++ m/library/shared-data.js
@@ -39,7 +39,8 @@ m.export_records=function(){
         }
     });
     var task_rec={};
-    var req={cmd:"export",table:tabledata,I1:m.I1,search:$('#keyword__ID').val()}
+    m.Table=tabledata;
+    var req={cmd:"export",table:m.Table,I1:m.I1,search:$('#keyword__ID').val()}
     $vm.request(req,function(N,i,txt){
         //console.log(i+"/"+N);
         $('#msg__ID').text((100*i/N).toFixed(0)+"%");
@@ -55,9 +56,9 @@ m.export_records=function(){
     function check(){
         if (req_count<2){
             setTimeout(function(){
-                console.log(req_count)
+                //console.log(req_count)
                 check();
-            },100);
+            },500);
         }
         else{
             combine_records()
@@ -66,9 +67,12 @@ m.export_records=function(){
     var combine_records=function(){
         var fields_ex=m.fields.replace("_Participant_ID","Participant_uid")
         var export_fields=fields_ex.split(',');
-        export_fields=export_fields.slice(6,export_fields.length-3);
+        export_fields=export_fields.slice(5,export_fields.length-3);
         //Participants export fields Specified in module-list
         var participant_export=$vm.module_list['participant-data'].participant_export;
+        if(participant_export==undefined){
+            participant_export="ID,Randomisation_Number,Subject_ID,Screening_Number,Subject_Initials,Gender,DOB"
+        }
         var participant_fields=participant_export.split(',');
         //Create empty object with all export fields. Participant and Task
         var empty_item={}
@@ -81,110 +85,46 @@ m.export_records=function(){
         var empty_item2={};
         //Loop through all participants and fill in task fields linked to them.
         //Put all in output_data object
-        for(var i=0;i<participant_rec.length;i++){
-            //console.log("PPP "+participant_rec[i].ID)
-            for (var k=0;k<task_rec.length;k++){
-                if(task_rec[k].Participant_uid==participant_rec[i].ID){
+        for(var ii=0;ii<participant_rec.length;ii++){
+            empty_item2={};
+            for (var kk=0;kk<task_rec.length;kk++){
+                if(task_rec[kk].Participant_uid==participant_rec[ii].ID){
                     //Get a new empty object
                     empty_item2=(JSON.parse(JSON.stringify(empty_item)));
-                    for( var l=0;l<participant_fields.length;l++){
-                        if(participant_rec[i].hasOwnProperty(participant_fields[l])){
-                            empty_item2[participant_fields[l]]=participant_rec[i][participant_fields[l]];
+                    for( var ll=0;ll<participant_fields.length;ll++){
+                        if(participant_rec[ii].hasOwnProperty(participant_fields[ll])){
+                            empty_item2[participant_fields[ll]]=participant_rec[ii][participant_fields[ll]];
                         }
                     }
-                    for( var l=0;l<export_fields.length;l++){
-                        if(task_rec[k].hasOwnProperty(export_fields[l])){
-                            empty_item2[export_fields[l]]=task_rec[k][export_fields[l]];
+                    for( var ll=0;ll<export_fields.length;ll++){
+                        if(task_rec[kk].hasOwnProperty(export_fields[ll])){
+                            empty_item2[export_fields[ll]]=task_rec[kk][export_fields[ll]];
                         }
                     }
                     output_data.push(empty_item2);
                     break;
                 }
+                else if(kk==task_rec.length-1){
+                    empty_item2={};
+                    for( var ll=0;ll<participant_fields.length;ll++){
+                        if(participant_rec[ii].hasOwnProperty(participant_fields[ll])){
+                            empty_item2[participant_fields[ll]]=participant_rec[ii][participant_fields[ll]];
+                        }
+                    }
+                    for( var ll=0;ll<export_fields.length;ll++){
+                        empty_item2[export_fields[ll]]="";
+                    }
+                    output_data.push(empty_item2)
+                }
             }
         }
-        console.log(output_data)
-        var tmp=JSON.stringify(output_data).replace(/ID/,"Participant ID").replace(/"off"/g,'"N"').replace(/"on"/g,'"Y"');
+        //console.log(output_data)
+        var tmp=JSON.stringify(output_data).replace(/ID/g,"Participant ID").replace(/"off"/g,'"N"').replace(/"on"/g,'"Y"');
         output_data=JSON.parse(tmp);
+        //console.log(JSON.stringify(output_data));
         $vm.download_csv(m.Table+".csv",output_data);
     }
     close_model__ID();
-    /*
-    var len=txt.length;
-            n_txt="["+txt.substring(5,len-9)+"]";
-            participant_rec=JSON.parse(n_txt);
-            //console.log(JSON.stringify(participant_rec))
-            //$vm.download_csv(m.Table+".csv",o);
-            close_model__ID();
-            m.Table=tabledata;
-            var req={cmd:"export",table:m.Table,I1:m.I1,search:$('#keyword__ID').val()}
-            open_model__ID();
-            $vm.request(req,function(N,i,txt){
-                //console.log("B"+i+"/"+N);
-                $('#msg__ID').text((100*i/N).toFixed(0)+"%");
-                if(i==-1){
-                    var len=txt.length;
-                    var data_rec="["+txt.substring(5,len-9)+"]";
-                    var o=JSON.parse(data_rec);
-                    var fields_ex=m.fields.replace("_Participant_ID","Participant_uid")
-                    var export_fields=fields_ex.split(',');
-                    for (var j=0;j<export_fields.length;j++){
-                        var tmp=export_fields[j].split('|')
-                        if (tmp.length>1) export_fields[j]=tmp[1];
-                    }
-                    //Add undefined values to first record (lowest Participant_uid) to get complete records for all entries. 
-                    o.sort(function(a, b) {
-                        return parseFloat(a.Participant_uid) - parseFloat(b.Participant_uid);
-                    });
-                    for (var j=2;j<export_fields.length-3;j++){
-                        //console.log(export_fields[j])
-                        if(export_fields[j].indexOf("_")!=0){
-                            if(o[0][export_fields[j]]==undefined){
-                                o[0][export_fields[j]]="";
-                            }
-                        }
-                    }
-                    //Order by m.fields
-                    export_fields=export_fields.slice(4,export_fields.length-3);
-                    console.log(JSON.stringify(export_fields))
-                    var oo=JSON.parse(JSON.stringify(o,export_fields));
-                    //console.log(JSON.stringify(oo))
-                    //Create an empty item so download.csv will create all headings
-                    var output_data=[];
-                    for(var ii=0;ii<participant_rec.length;ii++){
-                        if(oo.length>0){
-                            for (var k=0;k<oo.length;k++){
-                                if(oo[k].Participant_uid==participant_rec[ii].ID){
-                                    output_data.push(oo[k]);
-                                    break;
-                                }
-                                if(k==oo.length-1) {
-                                    var item={}
-                                    for(var l=0;l<export_fields.length;l++){
-                                        item[export_fields[l]]="";
-                                    }
-                                    item.Participant_uid=participant_rec[ii].ID;
-                                    output_data.push(item)
-                                }
-                            }    
-                        }
-                        else{
-                            var item={}
-                            for(var l=0;l<export_fields.length;l++){
-                                item[export_fields[l]]="";
-                            }
-                            item.Participant_uid=participant_rec[ii].ID;
-                            output_data.push(item)
-                        }
-                    }
-                    var tmp=JSON.stringify(output_data).replace(/Participant_uid/g,"Participant ID")
-                    output_data=JSON.parse(tmp);
-                    //console.log(JSON.stringify(output_data))
-                    $vm.download_csv(m.Table+".csv",output_data);
-                    close_model__ID();
-                }
-            });
-        }
-    }); */   
 }
 //-------------------------------------
 m.cell_render=function(records,I,field,td){
